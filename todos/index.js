c2d7d52c1439fd04ab1214e8b9eff7fe8ef38e17
Stephen <stephen.yin@outlook.com>
2020-02-16 21:55:37 +0800

    update

---
+++ todos/index.js
@@ -0,0 +1,2 @@
+import { Component } from 'https://raw.githack.com/hacker0limbo/react-redux-fake/master/src/react.js';
+import { createStore } from 'https://raw.githack.com/hacker0limbo/react-redux-fake/master/src/redux.js';
