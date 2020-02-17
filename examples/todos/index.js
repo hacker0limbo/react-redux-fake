75f34e8bfd0a0132f3349b8a66c47efe63bf2478
Stephen <stephen.yin@outlook.com>
2020-02-17 17:42:07 +0800

    finish

--- examples/todos/index.js
+++ examples/todos/index.js
@@ -7,7 +7,15 @@ const uuid = () => {
     Math.random().toString(36).substring(2, 15)
 }
 
-const todos = (state=[], action) => {
+const todosInitState = [
+  {
+    id: uuid(),
+    text: 'learn react',
+    completed: false
+  }
+]
+
+const todos = (state=todosInitState, action) => {
   switch(action.type) {
     case 'ADD_TODO':
       return [
@@ -46,35 +54,70 @@ class TodoApp extends Component {
     this.store = store
   }
 
+  handleClick(e) {
+    const target = e.target
+    const id = target.parentNode.dataset.id
+    const inputText = this.el.querySelector('input').value.trim()
+    if (target.classList.contains("add-todo")) {
+      this.props.handleAdd(inputText)
+    } else if (target.classList.contains("toogle-todo")) {
+      this.props.handleToogle(id)
+    } else if (target.classList.contains("delete-todo")) {
+      this.props.handleDelete(id)
+    }
+  }
+
   render() {
     return `
       <div>
         <h1>Todo App</h1>
         <input />
-        <button>add</button>
+        <button class="add-todo">add</button>
         <ul>
-          <div>
-            <li>1</li>
-            <button>toogle</button>
-            <button>delete</button>
-          </div>
-          <div>
-            <li>1</li>
-            <button>toogle</button>
-            <button>delete</button>
-          </div>
-          <div>
-            <li>1</li>
-            <button>toogle</button>
-            <button>delete</button>
-          </div>
+          ${this.props.todos.reduce((result, todo) => result + `
+            <div data-id=${todo.id}>
+              <li class=${todo.completed ? "completed" : ""}>${todo.text}</li>
+              <button class="toogle-todo">toogle</button>
+              <button class="delete-todo">delete</button>
+            </div>
+          `, "")}
         </ul>
       <div>
     `
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    todos: state.todos
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    handleAdd: text => {
+      dispatch({
+        type: 'ADD_TODO',
+        text
+      })
+    },
+    handleToogle: id => {
+      dispatch({
+        type: 'TOGGLE_TODO',
+        id
+      })
+    },
+    handleDelete: id => {
+      dispatch({
+        type: 'DELETE_TODO',
+        id
+      })
+    }
+  }
+}
+
 const root = document.querySelector('#root')
-const todoApp = new TodoApp()
+const TodoAppWithRedux = connect(mapStateToProps, mapDispatchToProps)(TodoApp)
+const todoApp = new TodoAppWithRedux()
 
 mount(todoApp, root)
